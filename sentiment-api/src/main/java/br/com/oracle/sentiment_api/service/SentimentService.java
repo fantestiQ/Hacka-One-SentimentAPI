2ca666ddafea75d65c57a5698309b8608a3df65d
fantestiQ <isac3539@gmail.com>
2026-01-12 07:47:41 -0300

    analise bug apos deploy

--- sentiment-api/src/main/java/br/com/oracle/sentiment_api/service/SentimentService.java
+++ sentiment-api/src/main/java/br/com/oracle/sentiment_api/service/SentimentService.java
@@ -2,6 +2,7 @@ package br.com.oracle.sentiment_api.service;
 
 import br.com.oracle.sentiment_api.dto.SentimentRequest;
 import br.com.oracle.sentiment_api.dto.SentimentResponse;
+import jakarta.validation.constraints.NotNull;
 import lombok.Value;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
@@ -16,17 +17,25 @@ public class SentimentService {
     // quando rodar localmente
     private final String DS_API_URL = "https://api-python-wymy.onrender.com/predict";
 
-    public SentimentResponse analyze(SentimentRequest request) {
+    public SentimentResponse analyze( @NotNull SentimentRequest request) {
+        Map<String, String> payload = Map.of("text", request.text());
 
-        Map<String, String> payload = Map.of(
-                "text", request.text()
-        );
+        try {
+            System.out.println("Tentando conectar em: " + DS_API_URL); // Log de debug
 
-        return restTemplate.postForObject(
-                DS_API_URL,
-                payload,
-                SentimentResponse.class
-        );
+            return restTemplate.postForObject(
+                    DS_API_URL,
+                    payload,
+                    SentimentResponse.class
+            );
+        } catch (Exception e) {
+            // Isso vai imprimir o erro REAL no console/logs da nuvem
+            System.err.println("ERRO AO CONECTAR NA API PYTHON: " + e.getMessage());
+            e.printStackTrace();
+
+            // Retorna um erro genérico ou relança para o Controller tratar
+            throw new RuntimeException("Falha na comunicação com serviço de IA", e);
+        }
     }
 }
 
